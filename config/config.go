8512a50dea5cd4cca684b30e8066c545419d61e5
amirhusseinSSoori <amirhusseinsppro75@gmail.com>
2024-04-24 22:43:57 +0330

    create project configuration

---
+++ config/config.go
@@ -0,0 +1,14 @@
+package config
+
+type Config struct {
+	App    App
+	Server Server
+}
+type App struct {
+	Name string
+}
+
+type Server struct {
+	Host string
+	Port string
+}
--- main.go
+++ main.go
@@ -1,17 +1,42 @@
 package main
 
 import (
+	"blog/config"
+	"fmt"
 	"net/http"
+
+	"github.com/spf13/viper"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	configs := configSet()
+
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
+			"message":  "pong",
+			"app name": viper.Get("App.Name"),
 		})
+
 	})
-	r.Run("localhost:8080") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	r.Run(fmt.Sprintf("%s:%s", configs.Server.Host, configs.Server.Port))
+}
+
+func configSet() config.Config {
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath("config")
+	if err := viper.ReadInConfig(); err != nil {
+		fmt.Println("Error reading the configs")
+	}
+	var configs config.Config
+
+	err := viper.Unmarshal(&configs)
+	if err != nil {
+		fmt.Printf("unable to decode into struct, %v", err)
+	}
+
+	return configs
 }
