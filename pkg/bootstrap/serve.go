8823913262746d6f4daffd8dc9da1c8bf4e1142a
amirhusseinSSoori <amirhusseinsppro75@gmail.com>
2024-05-06 22:16:29 +0330

    create static package

--- pkg/bootstrap/serve.go
+++ pkg/bootstrap/serve.go
@@ -4,12 +4,15 @@ import (
 	"blog/pkg/config"
 	"blog/pkg/html"
 	"blog/pkg/routing"
+	"blog/pkg/static"
 )
 
 func Serve() {
 	config.Set()
 
 	routing.Init()
+
+	static.LoadStatic(routing.GetRouter())
 
 	html.LoadHtml(routing.GetRouter())
 
---
+++ pkg/static/load.go
@@ -0,0 +1,10 @@
+package static
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func LoadStatic(router *gin.Engine) {
+	router.Static("/assets", "./assets")
+
+}
