c2b12efb43413b0bbf981d6e6342078489f7010a
amirhusseinSSoori <amirhusseinsppro75@gmail.com>
2024-06-09 21:18:42 +0330

    finalize home page

--- internal/modules/home/controllers/home_controllers.go
+++ internal/modules/home/controllers/home_controllers.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	ArticleService "blog/internal/modules/article/services"
+	"blog/pkg/html"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -19,12 +20,14 @@ func New() *Controller {
 }
 
 func (controller *Controller) Index(c *gin.Context) {
-	// html.Render(c, http.StatusOK, "modules/home/html/home", gin.H{
-	// 	"title": "HomePage",
-	// })
-
-	c.JSON(http.StatusOK, gin.H{
+	html.Render(c, http.StatusOK, "modules/home/html/home", gin.H{
+		"title":    "HomePage",
 		"featured": controller.articleService.GetFeaturedArticles(),
 		"stories":  controller.articleService.GetStoriesArticles(),
 	})
+
+	// c.JSON(http.StatusOK, gin.H{
+	// 	"featured": controller.articleService.GetFeaturedArticles(),
+	// 	"stories":  controller.articleService.GetStoriesArticles(),
+	// })
 }
